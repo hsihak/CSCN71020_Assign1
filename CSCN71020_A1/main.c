4656023b2d9b403bc3956e83047685da99fc96fd
Hangsihak Sin <hsin9104@conestogac.on.ca>
2022-11-01 13:05:37 -0400

    fix: rewrite subtract function

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,7 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
-
+void subtract();							// A function prototype added for subtract function to ensure that the compiler know that you are calling the function which includes function return type, proper type and number of arguments. 
 
 void main() {
 
@@ -20,6 +20,8 @@ void main() {
 	{
 	case 1:
 		add();
+	case 2:									// Case 2 added for user to choose option for subtract function 
+		subtract();							// Subtract function call added 
 	}
 
 }
@@ -44,4 +46,15 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract()								// Subtract function rewritten
+{
+	double num1, num2, result;
+	printf("Enter the first value: ");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value: ");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
